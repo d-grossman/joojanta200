54d5f2f2fef89f7da923ac4cdf6bedb460091190
dgrossman <dgrossman@iqt.org>
2017-06-02 14:55:07 +0000

    disable button when processing

--- includes/flask/static/js/index.js
+++ includes/flask/static/js/index.js
@@ -1,33 +1,46 @@
 $(function(){
-	$('#btnSignUp').click(function(){
-		
-		$.ajax({
-			url: '/',
-			data: $('form').serialize(),
-			type: 'POST',
-			success: function(response){
-		  	console.log(response);
+        $('#btnSignUp').click(function(){
+                var btn = $(this);
+
+                btn.prop('disabled',true);
+
+                $.ajax({
+                        url: '/',
+                        data: $('form').serialize(),
+                        type: 'POST',
+                        success: function(response){
+                        console.log(response);
 
                         var json = JSON.parse(response);
-			var _ret = json['ret'];
- 			var _msg = json['msg'];
+                        var _ret = json['ret'];
+                        var _msg = json['msg'];
 
-			console.log('response');
-			console.log(_ret);
-			console.log(_msg);
-	                if (_ret === 'True'){
+                        console.log('response');
+                        console.log(_ret);
+                        console.log(_msg);
+                        if (_ret === 'True'){
+
                         $("#topImage").html("<img height=\"294\" width=\"458\" src=\""+ json['topImage'] + "\"\>");
-				console.log('top');
+                                console.log('top');
                         $("#bottomImage").html("<img height=\"294\" width=\"458\" src=\""+ json['bottomImage'] + "\"\>");
-				console.log('bottom');
-			} else {
+                                console.log('bottom');
+                                btn.prop('disabled',false);
+
+                        } else {
+                        
                         $("#topImage").html("WARNING: " + _msg);
-				console.log('error');
-			}
-			},
-			error: function(error){
-				console.log(error);
-			}
-		});
-	});
+                                console.log('error');
+                                btn.prop('disabled',false);
+                        }
+                        },
+                        error: function(error){
+                                console.log(error);
+                                btn.prop('disabled',false);
+                        }
+                });
+
+                //$.post(/*...*/).complete(function(){
+                //      btn.prop('disabled',false);
+                //});
+        });
 });
