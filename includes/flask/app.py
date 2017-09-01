451305c9d1db22d868fcc77ca5e12da8c57b0516
dgrossman <dgrossman@xyxxl.com>
2017-08-31 22:03:59 -0700

    cleanup

--- includes/flask/app.py
+++ includes/flask/app.py
@@ -6,31 +6,40 @@ from subprocess import call
 
 import json
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def main():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/',methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def work_it():
     uri = request.form['uri']
 
     print(request.form)
     if uri:
-        print ('downloading uri {0}'.format(uri))
+        print('downloading uri {0}'.format(uri))
         try:
-            urllib.urlretrieve(uri,'/work/flask/static/top.jpg')
+            urllib.urlretrieve(uri, '/work/flask/static/top.jpg')
         except:
-            return json.dumps({'ret':'False','msg':'could not download {0}'.format(uri)})
+            return json.dumps({'ret': 'False',
+                               'msg': 'could not download {0}'.format(uri)})
 
         try:
-            call(['bash','/work/flask/doit.sh'])
-            return json.dumps({'ret':'True','topImage':'/static/top.jpg','bottomImage':'/static/bottom.bmp','msg':'success'})
+            call(['bash', '/work/flask/doit.sh'])
+            return json.dumps({'ret': 'True',
+                               'topImage': '/static/top.jpg',
+                               'bottomImage': '/static/bottom.bmp',
+                               'msg': 'success'})
         except:
-            return json.dumps({'ret':'False','msg':'failure running back end process'})
+            return json.dumps({'ret': 'False',
+                               'msg': 'failure running back end process'})
     else:
-        return json.dumps({'ret':'False','msg':'include a url for download'})
+        return json.dumps(
+            {'ret': 'False', 'msg': 'include a url for download'})
+
 
 @app.after_request
 def add_header(r):
@@ -38,11 +47,11 @@ def add_header(r):
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
     """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers[
+        "Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
     return r
-    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
--- includes/processAll.py
+++ includes/processAll.py
@@ -26,8 +26,10 @@ from PIL import Image as PILImage
 _CAFFE_ROOT = "/work/crfasrnn/caffe"
 
 # Model definition and model file paths
-_MODEL_DEF_FILE = "/work/crfasrnn/python-scripts/TVG_CRFRNN_new_deploy.prototxt.dg"  # Contains the network definition
-_MODEL_FILE = "/work/model/TVG_CRFRNN_COCO_VOC.caffemodel"  # Contains the trained weights. Download from http://goo.gl/j7PrPZ
+# Contains the network definition
+_MODEL_DEF_FILE = "/work/crfasrnn/python-scripts/TVG_CRFRNN_new_deploy.prototxt.dg"
+# Contains the trained weights. Download from http://goo.gl/j7PrPZ
+_MODEL_FILE = "/work/model/TVG_CRFRNN_COCO_VOC.caffemodel"
 
 sys.path.insert(0, _CAFFE_ROOT + "/python")
 sys.path.insert(0, _CAFFE_ROOT + "/python/caffe")
@@ -43,15 +45,16 @@ import caffe
 
 _MAX_DIM = 500
 
-def apply_mask(img,mask,color):
+
+def apply_mask(img, mask, color):
     len_x = mask.shape[0]
     len_y = mask.shape[1]
     for x in range(len_x):
         for y in range(len_y):
-            if mask[x,y] != color:
-                img[x,y][0] = 0
-                img[x,y][1] = 0
-                img[x,y][2] = 0
+            if mask[x, y] != color:
+                img[x, y][0] = 0
+                img[x, y][1] = 0
+                img[x, y][2] = 0
     return img
 
 
@@ -89,13 +92,14 @@ def crfrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
         model_def_file: File path of the Caffe model definition prototxt file
         model_file: File path of the trained model file (contains trained weights)
         gpu_device: ID of the GPU device. If using the CPU, set this to -1
-        inputs: List of images to be segmented 
+        inputs: List of images to be segmented
 
     Returns:
         The segmented image
     """
-    
-    assert os.path.isfile(model_def_file), "File {} is missing".format(model_def_file)
+
+    assert os.path.isfile(
+        model_def_file), "File {} is missing".format(model_def_file)
     assert os.path.isfile(model_file), ("File {} is missing. Please download it using "
                                         "./download_trained_model.sh").format(model_file)
 
@@ -110,8 +114,13 @@ def crfrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
     num_images = len(inputs)
     num_channels = inputs[0].shape[2]
     assert num_channels == 3, "Unexpected channel count. A 3-channel RGB image is exptected."
-    
-    caffe_in = np.zeros((num_images, num_channels, _MAX_DIM, _MAX_DIM), dtype=np.float32)
+
+    caffe_in = np.zeros(
+        (num_images,
+         num_channels,
+         _MAX_DIM,
+         _MAX_DIM),
+        dtype=np.float32)
     for ix, in_ in enumerate(inputs):
         caffe_in[ix] = in_.transpose((2, 0, 1))
 
@@ -119,7 +128,10 @@ def crfrnn_segmenter(model_def_file, model_file, gpu_device, inputs):
     out = net.forward_all(**{net.inputs[0]: caffe_in})
     end_time = time.time()
 
-    print("Time taken to run the network: {:.4f} seconds".format(end_time - start_time))
+    print(
+        "Time taken to run the network: {:.4f} seconds".format(
+            end_time -
+            start_time))
     predictions = out[net.outputs[0]]
 
     return predictions[0].argmax(axis=0).astype(np.uint8)
@@ -141,7 +153,8 @@ def run_crfrnn(input_file, output_file, gpu_device):
     image = np.array(image)
 
     palette = get_palette(256)
-    #PIL reads image in the form of RGB, while cv2 reads image in the form of BGR, mean_vec = [R,G,B] 
+    # PIL reads image in the form of RGB, while cv2 reads image in the form of
+    # BGR, mean_vec = [R,G,B]
     mean_vec = np.array([123.68, 116.779, 103.939], dtype=np.float32)
     mean_vec = mean_vec.reshape(1, 1, 3)
 
@@ -154,24 +167,25 @@ def run_crfrnn(input_file, output_file, gpu_device):
     cur_h, cur_w, cur_c = im.shape
     pad_h = _MAX_DIM - cur_h
     pad_w = _MAX_DIM - cur_w
-    im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)), mode='constant', constant_values=0)
+    im = np.pad(im, pad_width=((0, pad_h), (0, pad_w), (0, 0)),
+                mode='constant', constant_values=0)
 
     # Get predictions
-    segmentation = crfrnn_segmenter(_MODEL_DEF_FILE, _MODEL_FILE, gpu_device, [im])
+    segmentation = crfrnn_segmenter(
+        _MODEL_DEF_FILE, _MODEL_FILE, gpu_device, [im])
     segmentation = segmentation[0:cur_h, 0:cur_w]
 
     output_im = PILImage.fromarray(segmentation)
 
-    #compute and mask everything but humans
+    # compute and mask everything but humans
     output_im.putpalette(palette)
-    
 
-    real_output = apply_mask(image,segmentation,15)
+    real_output = apply_mask(image, segmentation, 15)
     output_im = PILImage.fromarray(real_output)
     output_im.save(output_file)
 
-    
-    #return (image,segmentation)
+    # return (image,segmentation)
+
 
 def resize_image(image):
     """ Resizes the image so that the largest dimension is not larger than 500 pixels.
@@ -193,14 +207,17 @@ def resize_image(image):
         else:
             ratio = float(_MAX_DIM) / width
         image = PILImage.fromarray(np.uint8(image))
-        image = image.resize((int(height * ratio), int(width * ratio)), resample=PILImage.BILINEAR)
+        image = image.resize(
+            (int(height * ratio), int(width * ratio)), resample=PILImage.BILINEAR)
         image = np.array(image)
 
     return image
 
+
 def run_ppl_only(in_filename, out_filename):
-    run_crfrnn(in_filename,out_filename,-1)
-    
+    run_crfrnn(in_filename, out_filename, -1)
+
+
 def find_images(topdir):
     retval = []
     exten = ['jpg', 'bmp', 'png']
@@ -212,26 +229,28 @@ def find_images(topdir):
                 #retval.append(os.path.join(dirpath, name))
     return retval
 
+
 def main():
     print 'Starting'
-    print sys.argv,len(sys.argv)
+    print sys.argv, len(sys.argv)
 
     if len(sys.argv) != 1:
-        run_crfrnn('/work/flask/static/top.jpg','/work/flask/static/bottom.bmp',-1)
+        run_crfrnn('/work/flask/static/top.jpg',
+                   '/work/flask/static/bottom.bmp', -1)
         return 0
 
     work_in = '/work/inputImages'
     work_out = '/work/outputImages'
     work_items = find_images(work_in)
-    
+
     for work_item in work_items:
-        in_filename = os.path.join(work_in,work_item)
+        in_filename = os.path.join(work_in, work_item)
         out_work_item = work_item.split('.')[0]
         out_work_item += '.png'
-        print(work_item,out_work_item)
-        out_filename = os.path.join(work_out,out_work_item)
-        print(in_filename,out_filename)
-        run_ppl_only(in_filename,out_filename)
+        print(work_item, out_work_item)
+        out_filename = os.path.join(work_out, out_work_item)
+        print(in_filename, out_filename)
+        run_ppl_only(in_filename, out_filename)
 
 
 if __name__ == '__main__':
